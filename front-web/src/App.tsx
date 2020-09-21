d3a46522af8d863aff68a7793214af497647328b
Gabriel Muniz <gsmuniz17@gmail.com>
2020-09-21 10:57:20 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <h1>
       <Header />
+      <Home />
     </h1>
   );
 }
